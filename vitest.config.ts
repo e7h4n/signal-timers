5974f42005eaf9d9fd1214a22be0e4623e71fc30
e7h4n <perfectworks@gmail.com>
2024-09-19 16:32:02 +0800

    add coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        environment: 'happy-dom'
+        environment: 'happy-dom',
+        coverage: {
+            reporter: ['text', 'json-summary', 'json', 'cobertura'],
+            provider: 'v8'
+        },
     },
 })
